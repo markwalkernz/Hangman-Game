91f6c32aa25d8dd2e51eab6528e659a5163b0d7a
markwalkernz <markwalkernz@gmail.com>
2017-09-12 15:50:52 -0500

    added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 // Week 3 coding boot camp homework.
 
 // variables
-var dictionary = ["argentina", "bolivia", "brazil", "chile", "colombia", "ecuador", "guyana", "paraguay", "peru", "suriname", "uruguay", "venezuela"];
+var dictionary = ["ARGENTINA", "BOLIVIA", "BRAZIL", "CHILE", "COLOMBIA", "ECUADOR", "GUYANA", "PARAGUAY", "PERU", "SURINAME", "URUGUAY", "VENEZUELA"];
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var wins = 0;
-var lastAnswer = "";
+var lastAnswer = "START";
 var remainingGuesses = 12;
 var arrayWrongGuesses = []
 var answer = "";
@@ -11,6 +12,7 @@ var arrayAnswer = [];
 var arrayCorrectGuesses = [];
 var correctGuesses = "";
 var gameOver = false;
+var imgSrc = "assets/images/START.png";
 
 // function to reset the game
 function resetGame() {
@@ -45,35 +47,47 @@ function resetGame() {
 	document.getElementById("current-word").innerHTML = correctGuesses
 	document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
 	document.getElementById("letters-guessed").innerHTML = arrayWrongGuesses;
-}
+	document.getElementById("last-answer").innerHTML = lastAnswer;
+	
+	// update the image on screen
+	imgSrc = "assets/images/" + lastAnswer + ".png"
+	document.getElementById("flag-image").src = imgSrc;
 
+} // end of resetGame function
+
+
+// start of the game
 // reset the game to initialise it
 resetGame();
 
 // enter guess
 document.onkeyup = function(event) {
 
-	// store key press as a variable
+	// store key press as a variable and convert to upper case
 	var guess = event.key;
+	guess = guess.toUpperCase();
+
 	console.log("key pressed: " + guess);
 
 	// initialise correct guess and set it to false
 	var isCorrectGuess = false;
 
-	// check that guess is a letter and convert to lower case
-	if (guess.toLowerCase() != guess.toUpperCase()) {
-		console.log("guess is a letter");
-		guess = guess.toLowerCase();
-		console.log("guess is " + guess);
+	// check that guess is a letter
+	if (alphabet.indexOf(guess) < 0) {
 
-		// then check if the letter has been guessed before
+		alert("Your guess is not a letter, please try again");
+	}
+
+	// if the guess is a letter
+	else {
+
+		// check if the letter has been guessed before
 		if (arrayWrongGuesses.indexOf(guess) >= 0 || arrayCorrectGuesses.indexOf(guess) >= 0) {
-			alert("already guessed " + guess + ", please try again.");
+			alert("You have already guessed " + guess + ", please try again.");
 		}
 
 		// if the letter has not been guessed before
 		else {
-			console.log("not guessed yet");
 
 			// for each letter in the answer
 			for (var i = 0; i < answer.length; i++) {
@@ -84,28 +98,31 @@ document.onkeyup = function(event) {
 					// if it is, put the letter in the current word being shown
 					arrayCorrectGuesses[i] = guess;
 					correctGuesses = arrayCorrectGuesses.join("");
-					document.getElementById("current-word").innerHTML = correctGuesses;
-					
+										
 					// set correct guess to true
 					isCorrectGuess = true;
+
+					//show the results on screen
+					document.getElementById("current-word").innerHTML = correctGuesses;
+
 				}
 			}
 				
 			// if the current guess is not in the answer
 			if (isCorrectGuess == false) {
+
 				// reduce the number of guesses remaining by 1
 				remainingGuesses = remainingGuesses - 1;
-				document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+				
 
 				// show the letter as already guessed
 				arrayWrongGuesses.push(guess);
+				
+				// show the results on screen
+				document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
 				document.getElementById("letters-guessed").innerHTML = arrayWrongGuesses;
 			}
 		}
-	}
-
-	else {
-		alert("Your guess is not a letter, please try again");
 	}
 
 	// check if the whole word has been guessed
@@ -113,13 +130,15 @@ document.onkeyup = function(event) {
 		wins = wins +1;
 		lastAnswer = answer;
 		resetGame();
-		// change image
 	}
 
 	// check if remaining guesses is zero
 	if (remainingGuesses == 0) {
 		alert("Sorry, no more guesses remaining. The correct answer was " + answer + ".");
+		lastAnswer = answer;
 		resetGame();
 	}
 
-}; // end keyup
+}; // end of keyup function
+
+// end of the game
